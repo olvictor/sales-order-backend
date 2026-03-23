2f351d9c39d594cf1abd6e46223d63ce4fd1a03f
victorolv <victorolvdev@gmail.com>
2026-03-23 14:06:27 -0300

    fix: main.ts

--- srv/main.ts
+++ srv/main.ts
@@ -31,45 +31,7 @@ export default (service : Service) => {
         if(result.hasError){
             return request.reject(400,result.error?.message as string)
         }
-        // const items: SalesOrderItems = request.data.params;
-
-        // if(!customer_id) return request.reject(400,'Customer inválido.')
-        // if(!items || request.data.params.items?.lenght == 0) return request.reject(400,"Items inválidos.");
-
-
-        // const customerQuery = SELECT.one.from("sales.Customers").where({id:customer_id})
-
-        // const customer = await cds.run(customerQuery);
-
-        // if(!customer) return request.reject(404,"Customer não encontrado.")
-
-        // const productsId = items.map((item: SalesOrderItem) => item.product_id)
-        // const productsIdQuery = SELECT.from("sales.Produtcs").where( {id: productsId})
-
-        // const products:Products = await cds.run(productsIdQuery);
-
-        // for(const item of items){
-        //     const dbProduct = products.find(product => product.id === item.product_id);
-
-        //     if(!dbProduct) return request.reject(404,`Produto ${item.product_id} não encontrado.`);
-
-        //     if(dbProduct.stock === 0) return request.reject(400,`O ${dbProduct.name}(${dbProduct.id} - está sem estoque.)`)
-        // }
-
-        // let totalAmout = 0;
-
-        // items.filter(item =>{
-        //     totalAmout += (item.price as number)  * (item.quantity as number)
-        // })
-        
-        // request.data.totalAmout = totalAmout;
-
-
-        // let desconto = totalAmout * (10/100);
-        // if(totalAmout > 30000){
-        //     let desconto = totalAmout * (10/100);
-        //     totalAmout = totalAmout - desconto;
-        // }
+       
 
           request.data.totalAmount = result.totalAmount;
      });   
