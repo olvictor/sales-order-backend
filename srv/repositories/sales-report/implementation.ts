723b9508c3e321c109a9b88f266896092672667f
victorolv <victorolvdev@gmail.com>
2026-03-27 17:27:23 -0300

    feat: create findByCustomerId method in SalesRepository

--- srv/repositories/sales-report/implementation.ts
+++ srv/repositories/sales-report/implementation.ts
@@ -1,4 +1,3 @@
-/* eslint-disable max-lines-per-function */
 import cds from '@sap/cds';
 
 import { ExpectedResult } from '@models/db/types/SalesReportByDays';
@@ -8,30 +7,51 @@ import { SalesReportRepository } from '@/repositories/sales-report/protocol';
 
 export class SalesReportRepositoryImpl implements SalesReportRepository {
     public async findByDays(days: number): Promise<SalesReportModel[] | null> {
-        console.log(days);
         const today = new Date().toISOString();
         const subtractedDays = new Date();
         subtractedDays.setDate(subtractedDays.getDate() - days);
         const subtractedDaysISOString = subtractedDays.toISOString();
 
-        const sql = SELECT.from('sales.SalesOrderHeaders')
-            .columns(
-                'id as salesOrderId',
-                'totalAmount as salesOrderTotalAmount',
-                'customer.id as customerId',
-                // eslint-disable-next-line quotes
-                `customer.firstName || ' ' || customer.lastName as customerFullName`
-            )
-            .where({
-                createdAt: {
-                    between: subtractedDaysISOString,
-                    and: today
-                }
-            });
+        const sql = this.getReportBaseSql().where({
+            createdAt: {
+                between: subtractedDaysISOString,
+                and: today
+            }
+        });
 
         const salesReports = await cds.run(sql);
         console.log(salesReports);
-        if (salesReports.lenght === 0) {
+        if (salesReports.length === 0) {
+            return null;
+        }
+        return this.mapReportResult(salesReports);
+    }
+
+    public async findByCustomerId(customerId: string): Promise<SalesReportModel[] | null> {
+        const sql = this.getReportBaseSql().where({
+            customer_id: customerId
+        });
+
+        const salesReports = await cds.run(sql);
+
+        if (salesReports.length === 0) {
+            return null;
+        }
+        return this.mapReportResult(salesReports);
+    }
+
+    private getReportBaseSql(): cds.ql.SELECT<unknown, unknown> {
+        return SELECT.from('sales.SalesOrderHeaders').columns(
+            'id as salesOrderId',
+            'totalAmount as salesOrderTotalAmount',
+            'customer.id as customerId',
+            // eslint-disable-next-line quotes
+            `customer.firstName || ' ' || customer.lastName as customerFullName`
+        );
+    }
+
+    private mapReportResult(salesReports: ExpectedResult[]): SalesReportModel[] | null {
+        if (salesReports.length === 0) {
             return null;
         }
         return salesReports.map((salesReport: ExpectedResult) =>
--- srv/repositories/sales-report/protocol.ts
+++ srv/repositories/sales-report/protocol.ts
@@ -3,4 +3,5 @@ import { SalesReportModel } from '@/models/sales-report';
 
 export interface SalesReportRepository {
     findByDays(days: number): Promise<SalesReportModel[] | null>;
+    findByCustomerId(customerId: string): Promise<SalesReportModel[] | null>;
 }
